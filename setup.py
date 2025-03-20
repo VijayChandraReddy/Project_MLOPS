10fd85bb0b6c797de98d74e9f84f3416fc2567cf
vijay <vc741755@gmail.com>
2025-03-20 15:47:40 -0400

    Project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name= 'src',
+    version='0.0.1',
+    author='Vijay',
+    author_email='vc741755@gmail.com',
+    packages=find_packages()
+)
